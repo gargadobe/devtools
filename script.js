69ef0449bdca91b70adc6c0ea73c8db7c3bfed6f
neeraj <garg@adobe.com>
2024-02-28 12:14:05 +0530

    fix

--- script.js
+++ script.js
@@ -44,14 +44,15 @@ function showTree() {
 }
 
 function createTree(obj, path = '') {
-    let html = '';
+    let html = '<div class="json-brackets">{</div>';
     for (let key in obj) {
         if (typeof obj[key] === 'object') {
-            html += `<details open><summary>${key}</summary><ul>${createTree(obj[key], path + key + '.')}</ul></details>`;
+            html += `<details open class="json-object"><summary class="json-key">${key}</summary><div class="json-brackets">{</div><ul class="json-object-content">${createTree(obj[key], path + key + '.')}</ul><div class="json-brackets">}</div></details>`;
         } else {
-            html += `<li><span class="key">${key}:</span> <span class="value" contenteditable="true" data-path="${path + key}">${obj[key]}</span></li>`;
+            html += `<li class="json-item"><div class="json-key-value"><span class="json-key">${key}:</span> <span class="json-value" contenteditable="true" data-path="${path + key}">${obj[key]}</span></div></li>`;
         }
     }
+    html += '<div class="json-brackets">}</div>';
     return html;
 }
 
